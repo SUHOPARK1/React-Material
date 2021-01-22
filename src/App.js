8bed2cb9de23847158306306d703c3a1d9745c62
SUHOPARK1 <qkrtnghsla@naver.com>
2021-01-22 14:12:47 +0900

    2021.01.22

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
-import Login from './cmm/Login'
-
+/* import Blog from './bbs/pages/Blog' */
+/* import Album from './bbs/pages/Album' */
+/* import BasicRouter from './tst/BasicRouter' */
+/* import Checkout from './sts/pages/Checkout' */
+/* import Dashboard from './sts/pages/Dashboard' */
+/* import SignInSide from './sym/pages/SignInSide' */
+/* import Pricing from './uss/pages/Pricing' */
+/* import SiginUp from './uss/pages/StdnJoin' */
+import Home from './cmm/pages/Home'
 export default function App() {
   return (
-    <Login/>
+    <Home/>
   )
 }
+
